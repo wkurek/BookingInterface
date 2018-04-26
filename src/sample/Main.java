f3ee765ae6a6bf0dfef6281f0e1269e7da552342
wkurek <w.kurek2@gmail.com>
2018-04-26 15:37:40 +0200

    Menu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import sample.controller.ClientController;
 import sample.controller.ReservationController;
+import sample.controller.RootController;
 import sample.model.Reservation;
 import sample.model.ReservationDAO;
 import sample.util.DateFormatter;
@@ -26,8 +27,6 @@ public class Main extends Application {
         primaryStage.setTitle("Booking");
 
         initRootLayout();
-        initClientView();
-
     }
 
     private void initRootLayout() {
@@ -40,34 +39,14 @@ public class Main extends Application {
             primaryStage.setScene(primaryScene);
             primaryStage.show();
 
+            RootController clientController = loader.getController();
+            clientController.setStage(primaryStage);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void initClientView() {
-        try {/*
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/ClientView.fxml"));
-            AnchorPane anchorPane = loader.load();
-
-            ClientController clientController = loader.getController();
-            clientController.setStage(primaryStage);*/
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/ReservationView.fxml"));
-            AnchorPane anchorPane = loader.load();
-
-            ReservationController controller = loader.getController();
-            controller.setStage(primaryStage);
-
-
-
-
-            rootLayout.setCenter(anchorPane);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         launch(args);
--- src/sample/controller/RootController.java
+++ src/sample/controller/RootController.java
@@ -1,4 +1,97 @@
 package sample.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sample.Main;
+
+import java.io.IOException;
+
 public class RootController {
+    private Stage stage;
+
+    @FXML
+    private BorderPane rootLayout;
+
+    @FXML
+    private void onNewReservationClicked() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/ReservationForm.fxml"));
+            AnchorPane page = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Create Reservation");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(stage);
+
+            ReservationFormController controller = loader.getController();
+            controller.setStage(dialogStage);
+
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+            dialogStage.show();
+        } catch (IOException e) {}
+    }
+
+    @FXML
+    private void onNewClientClicked() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/ClientForm.fxml"));
+            AnchorPane page = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Create Client");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(stage);
+
+            ClientFormController controller = loader.getController();
+            controller.setStage(dialogStage);
+
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+            dialogStage.show();
+        } catch (IOException e) {}
+    }
+
+    @FXML
+    private void onShowReservationsClicked() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/ReservationView.fxml"));
+            AnchorPane anchorPane = loader.load();
+
+            ReservationController controller = loader.getController();
+            controller.setStage(stage);
+
+            rootLayout.setCenter(anchorPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onShowClientsClicked() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/ClientView.fxml"));
+            AnchorPane anchorPane = loader.load();
+
+            ClientController clientController = loader.getController();
+            clientController.setStage(stage);
+
+            rootLayout.setCenter(anchorPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 }
